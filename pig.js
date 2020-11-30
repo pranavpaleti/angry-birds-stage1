f7ed99fb7e20a584e1f1d7357719ee9e6a265690
pranavpaleti <73649648+pranavpaleti@users.noreply.github.com>
2020-11-30 18:48:29 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,19 @@
+class Pig{
+    constructor(x,y){
+        this.body=Bodies.rectangle(x,y,50,50)
+        this.width=50
+        this.height=50
+        World.add(myWorld,this.body)
+    }
+    display(){
+    var pos=this.body.position
+    var angle=this.body.angle
+    push()
+    translate(pos.x,pos.y)
+    rotate(angle)
+    rectMode(CENTER)
+    fill("green")
+    rect(0,0,this.width,this.height)
+    pop()
+    }
+}
